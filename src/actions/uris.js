07bdf9e5a744364c86d00aeafc418ba6384f4e5d
KoushikKumar <koushikkumar222@gmail.com>
2017-03-27 01:46:39 +0530

    Pointing to production server uri

--- src/actions/uris.js
+++ src/actions/uris.js
@@ -1,2 +1,2 @@
-export const STOCK_WATCH_SERVER_URI = "https://stock-watch-koushikkumarv.c9users.io";
+export const STOCK_WATCH_SERVER_URI = "https://stock-watch-server-kkv.herokuapp.com";
 export const GET_STOCK_DATA_URI = `${STOCK_WATCH_SERVER_URI}/get-stock-data`
